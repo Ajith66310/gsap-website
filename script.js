738940893d42a9d4ee23ca044d28020553c6fdf7
Ajith k v <ajith66310@gmail.com>
2025-10-12 20:08:16 +0530

    feat: using gsap to animate nav,hero,brands logo,using tailwind removed normal css ,responsive in sm,md,lg

---
+++ script.js
@@ -0,0 +1,63 @@
+    const openSidebar = document.getElementById('openSidebar');
+    const closeSidebar = document.getElementById('closeSidebar');
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
+
+    openSidebar.addEventListener('click', () => {
+      sidebar.classList.remove('-translate-x-full');
+      overlay.classList.remove('opacity-0', 'pointer-events-none');
+    });
+
+    const closeMenu = () => {
+      sidebar.classList.add('-translate-x-full');
+      overlay.classList.add('opacity-0', 'pointer-events-none');
+    };
+
+    closeSidebar.addEventListener('click', closeMenu);
+    overlay.addEventListener('click', closeMenu);
+
+
+    // gsap
+   const page1Animation = ()=>{
+     var tl = gsap.timeline()
+
+    tl.from("nav h1,nav h4,nav button",{
+      y:-40,
+      opacity:0,
+      delay:0.7,  
+      stagger:0.15,
+    })
+    
+    tl.from(".center-part1 h1",{
+      x:-200,
+      opacity:0,
+      duration:0.5
+    })
+    tl.from(".center-part1 p",{
+      x:-100,
+      opacity:0,
+      duration:0.4
+    })
+    
+    tl.from(".center-part1 button",{
+      opacity:0,
+      duration:0.4
+    })
+    
+    tl.from(".center-part2 img",{
+      opacity:0,
+      x:200,
+      duration:0.4
+    },"-=1")
+    tl.from(".brands img",{
+      opacity:0,
+      y:30,
+      stagger:0.15,
+      duration:0.7,
+    })
+
+   }
+
+   page1Animation()
+
+   
