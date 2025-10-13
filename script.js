219eec9493d4565d3711dc497ac5cf502d911f2e
Ajith k v <ajith66310@gmail.com>
2025-10-13 10:50:40 +0530

    feat: implemented scroll-trigger & animated page-2

--- script.js
+++ script.js
@@ -49,15 +49,62 @@
       x:200,
       duration:0.4
     },"-=1")
-    tl.from(".brands img",{
-      opacity:0,
-      y:30,
-      stagger:0.15,
-      duration:0.7,
-    })
+    
 
    }
 
    page1Animation()
 
+const page2Animation = ()=>{
+
+  var tl2 = gsap.timeline({
+    scrollTrigger:{
+      trigger:".section2 ",
+      scroller:"body",
+      scrub:2,
+      start:"top 50%",
+      end:"top 0"
+    }
+   })
    
+   tl2.from(".brands img",{
+      opacity:0,
+      y:30,
+      stagger:0.15,
+      duration:0.7,
+    })
+    tl2.from(".services",{
+    x:-100,
+    opacity:0,
+    duration:0.5,
+  })
+  
+  tl2.from(".line1#left",{
+    x:-300,
+    opacity:0,
+    duration:1,
+    delay:1,
+  },"anim1")
+  
+  tl2.from(".line1#right",{
+    x:300,
+    opacity:0,
+    duration:1,
+  },"anim1")
+  
+  tl2.from(".line2#left",{
+    x:-300,
+    delay:1,
+    opacity:0,
+    duration:1,
+  },"anim2")
+  
+  tl2.from(".line2#right",{
+    x:300,
+    opacity:0,
+    duration:1,
+  },"anim2")
+  
+}
+
+page2Animation()
